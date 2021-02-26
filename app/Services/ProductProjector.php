188127054177c0d48954be8e7f1ed1eb234e7072
pscibisz <pscibisz@getresponse.com>
2021-02-26 13:30:03 +0100

    projector

--- app/Services/ProductProjector.php
+++ app/Services/ProductProjector.php
@@ -9,6 +9,27 @@ class ProductProjector
 {
     public static function convert(Collection $collection): array
     {
-        return $collection->toArray();
+        $response = [];
+        foreach ($collection as $key => $product) {
+            $response[$key] = [
+                'id'          => $product->id,
+                'type'        => $product->type,
+                'description' => $product->description,
+                'image_url'   => $product->image_url,
+                'variant'     => [
+                    'id'        => $product->v_id,
+                    'price'     => $product->price,
+                    'rate'      => $product->rate,
+                    'image_url' => $product->v_image_url,
+                    'attribute' => [
+                        'id'      => $product->a_id,
+                        'a_name'  => $product->a_name,
+                        'a_value' => $product->a_value
+                    ]
+                ]
+            ];
+        }
+
+        return $response;
     }
 }
